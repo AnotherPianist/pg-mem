70f804eced9de12e953cf38149514e2ddbbdf589
ThePianist <cafarimo@outlook.com>
2024-11-17 23:11:39 -0300

    Build deno [autogenerated commit]

--- .deno/index.ts
+++ .deno/index.ts
@@ -1,4 +1,4 @@
-export { newDb } from './db.ts';
-export { enableStatementLocationTracking } from './parser/parse-cache.ts';
 export { replaceQueryArgs$ } from './adapters/index.ts';
+export { deserialize, newDb } from './db.ts';
 export * from './interfaces.ts';
+export { enableStatementLocationTracking } from './parser/parse-cache.ts';
